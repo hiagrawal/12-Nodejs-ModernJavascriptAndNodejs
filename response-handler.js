03d63c9b7d491b569942f24e6befbbfa29d1df3c
hiagrawal <agrawal.hina13@gmail.com>
2021-11-23 21:32:06 +0530

    using promises instead of callback hells in core API functions like read/write file

--- response-handler.js
+++ response-handler.js
@@ -1,5 +1,10 @@
 //const fs = require('fs');
-import fs from 'fs';
+// import fs from 'fs';
+
+//when using promises
+//const fs = require('fs').promises;
+import fs from 'fs/promises';
+
 import path, {dirname} from 'path';
 import { fileURLToPath } from 'url';
 
@@ -12,7 +17,18 @@ export const resHandler = (req, res, next) => {
     //   res.send(data);
     // });
 
-    res.sendFile(path.join(__dirname, "my-page.html"));
+    //how to use globally defined variables when using es6
+    //res.sendFile(path.join(__dirname, "my-page.html"));
+
+    //using promises in core API functions, instead of callback, we can chain like promises
+    fs.readFile('my-page.html', 'utf8')
+    .then((data) => {
+        res.send(data);
+      })
+      .catch(err => {
+          console.log(err)
+      });
+
 }
 
 //export default resHandler;
